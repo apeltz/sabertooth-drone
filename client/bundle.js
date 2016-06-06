4acacfe34a1f279f6cfb523fe53e266602e738bf
apeltz <aaron.peltz@gmail.com>
2016-06-05 21:46:13 -0700

    added optional services to gattServiceList

--- client/bundle.js
+++ client/bundle.js
@@ -1629,10 +1629,14 @@ var bluetoothMap = {
 			}
 		}
 	},
-	gattServiceList: ['alert_notification', 'automation_io', 'battery_service', 'blood_pressure', 'body_composition', 'bond_management', 'continuous_glucose_monitoring', 'current_time', 'cycling_power', 'cycling_speed_and_cadence', 'device_information', 'environmental_sensing', 'generic_access', 'generic_attribute', 'glucose', 'health_thermometer', 'heart_rate', 'human_interface_device', 'immediate_alert', 'indoor_positioning', 'internet_protocol_support', 'link_loss', 'location_and_navigation', 'next_dst_change', 'phone_alert_status', 'pulse_oximeter', 'reference_time_update', 'running_speed_and_cadence', 'scan_parameters', 'tx_power', 'user_data', 'weight_scale']
+	gattServiceList: ["9a66fb00-0800-9191-11e4-012d1540cb8e",
+"9a66fd21-0800-9191-11e4-012d1540cb8e",
+"9a66fd51-0800-9191-11e4-012d1540cb8e",
+"9a66fa00-0800-9191-11e4-012d1540cb8e",'alert_notification', 'automation_io', 'battery_service', 'blood_pressure', 'body_composition', 'bond_management', 'continuous_glucose_monitoring', 'current_time', 'cycling_power', 'cycling_speed_and_cadence', 'device_information', 'environmental_sensing', 'generic_access', 'generic_attribute', 'glucose', 'health_thermometer', 'heart_rate', 'human_interface_device', 'immediate_alert', 'indoor_positioning', 'internet_protocol_support', 'link_loss', 'location_and_navigation', 'next_dst_change', 'phone_alert_status', 'pulse_oximeter', 'reference_time_update', 'running_speed_and_cadence', 'scan_parameters', 'tx_power', 'user_data', 'weight_scale']
 };
 
 module.exports = bluetoothMap;
+
 },{}],4:[function(require,module,exports){
 "use strict";
 
